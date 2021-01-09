494a0034ac7aeaba84b295f3b5242fd92a9982b0
sirbobies <54443117+sirbobies@users.noreply.github.com>
2021-01-09 12:56:38 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 
 main(){
   systemctl stop firewalld
-  ssserver -m aes-256-cfb -p 12345 -k abcedf --manager-address 127.0.0.1:4000 --user nobody -d start
+  ssserver -m aes-256-cfb -p 12345 -k abcedf --manager-address 127.0.0.1:6001 --user nobody -d start
   pm2 --name "ss" -f start ssmgr -x -- -c ~/.ssmgr/ss.yml
   pm2 --name "webgui" -f start ssmgr -x -- -c ~/.ssmgr/webgui.yml
 }
